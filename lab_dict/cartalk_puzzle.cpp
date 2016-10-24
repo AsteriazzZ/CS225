c6388744e0554a4d7bf8915f355b6efae6d71458
zzhan145 <zzhan145@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-10-24 05:33:03 +0000

    “lab_dict”

--- lab_dict/cartalk_puzzle.cpp
+++ lab_dict/cartalk_puzzle.cpp
@@ -24,24 +24,38 @@ vector<StringTriple> cartalk_puzzle(PronounceDict d,
                                     const string& word_list_fname)
 {
     /* Your code goes here! */
-    /**
-     ifstream words(word_list_fname);
-     string word;
-     vector<StringTriple> result;
-     if(words.is_open()){
-    	while(getline(words, word)){
-     if(word.length() == 5){
-     string temp1 = word.substr(1, word.length()-1);
-     string temp2 = word.substr(0, 1);
-     string temp3 = word.substr(2, word.length()-2);
-     temp2.append(temp3);
-     if(d.homophones(temp1, temp2)){
-     result.push_back(make_tuple(word, temp1, temp2));
-     }
-     }
-     }
-     }
-     return result;
-     */
-    return vector<StringTriple>();
+    
+//     ifstream words(word_list_fname);
+//     string word;
+//     vector<StringTriple> result;
+//     if(words.is_open()){
+//    	while(getline(words, word)){
+//     if(word.length() == 5){
+//     string temp1 = word.substr(1, word.length()-1);
+//     string temp2 = word.substr(0, 1);
+//     string temp3 = word.substr(2, word.length()-2);
+//     temp2.append(temp3);
+//     if(d.homophones(temp1, temp2)){
+//     result.push_back(make_tuple(word, temp1, temp2));
+//     }
+//     }
+//     }
+//     }
+//     return result;
+    vector<StringTriple> ret;
+    ifstream words(word_list_fname);
+    string word;
+    if(words.is_open()){
+        while(getline(words, word)){
+            if (word.length() > 1){
+                string tp1 = word.substr(1);
+                string tp2 = word[0] + word.substr(2);
+                if (d.homophones(word, tp1) && d.homophones(word, tp2)){
+                    StringTriple word_trip = make_tuple(word, tp1, tp2);
+                    ret.push_back(word_trip);
+                }
+            }
+        }
+    }
+    return ret;
 }
