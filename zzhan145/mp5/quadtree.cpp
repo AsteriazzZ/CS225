dbeddcfe247ff7ba5e393ae0b93a0b422dbb696d
zzhan145 <zzhan145@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-10-31 05:29:48 +0000

    mp5 submission

--- zzhan145/mp5/quadtree.cpp
+++ zzhan145/mp5/quadtree.cpp
@@ -8,32 +8,40 @@
 
 //mp5.1
 void Quadtree::buildTree(PNG const &source, int resolution){
-    if(root!=NULL) clear(root);
+    if(root != NULL) clear(root);
     
     this->resolution = resolution;
     buildTree(root, source, 0, 0, resolution);
 }
 
 //helper function for buildTree
-void Quadtree::buildTree(QuadtreeNode *&curNode, PNG const &source, int x, int y, int resolution){
-    if(resolution == 1){
-        curNode = new QuadtreeNode();
-        curNode->element = *source(x,y);
+void Quadtree::buildTree(QuadtreeNode *&subRoot, PNG const &source, int x, int y, int resol){
+    
+    if (resol == 1){
+        subRoot = new QuadtreeNode();
+        subRoot->element = *source(x,y);
         return;
     }
     
-    curNode = new QuadtreeNode();
-    buildTree(curNode->nwChild, source, x, y, resolution/2);
-    buildTree(curNode->neChild, source, x+resolution/2, y, resolution/2);
-    buildTree(curNode->swChild, source, x, y+resolution/2, resolution/2);
-    buildTree(curNode->seChild, source, x+resolution/2, y+resolution/2, resolution/2);
+    subRoot = new QuadtreeNode();
+    buildTree(subRoot->nwChild, source, x, y, resol/2);
+    buildTree(subRoot->neChild, source, x+resol/2, y, resol/2);
+    buildTree(subRoot->swChild, source, x, y+resol/2, resol/2);
+    buildTree(subRoot->seChild, source, x+resol/2, y+resol/2, resol/2);
     
-    if(curNode->nwChild!=NULL){
-        curNode->element.red = (curNode->nwChild->element.red + curNode->neChild->element.red + curNode->seChild->element.red + curNode->swChild->element.red)/4;
-        curNode->element.blue = (curNode->nwChild->element.blue + curNode->neChild->element.blue + curNode->seChild->element.blue + curNode->swChild->element.blue)/4;
-        curNode->element.green = (curNode->nwChild->element.green + curNode->neChild->element.green + curNode->seChild->element.green + curNode->swChild->element.green)/4;
+    if (subRoot->nwChild!=NULL){
+        subRoot->element.red = (subRoot->nwChild->element.red + subRoot->neChild->element.red
+                                + subRoot->seChild->element.red + subRoot->swChild->element.red)/4;
+        subRoot->element.blue = (subRoot->nwChild->element.blue + subRoot->neChild->element.blue
+                                + subRoot->seChild->element.blue + subRoot->swChild->element.blue)/4;
+        subRoot->element.green = (subRoot->nwChild->element.green + subRoot->neChild->element.green
+                                + subRoot->seChild->element.green + subRoot->swChild->element.green)/4;
     }
 }
+
+/*****************************
+ * The big three
+ ****************************/
 
 Quadtree::Quadtree(){
     // no argument constructor
@@ -41,10 +49,10 @@ Quadtree::Quadtree(){
     resolution = 1;
 }
 
-Quadtree::Quadtree(PNG const &source, int resolution){
+Quadtree::Quadtree(PNG const &source, int resol){
     // two argument constructor
     root = new QuadtreeNode();
-    buildTree(source, resolution);
+    buildTree(source, resol);
 }
 
 Quadtree::Quadtree(Quadtree const &other){
@@ -77,20 +85,16 @@ Quadtree::~Quadtree(){
 }
 
 //helper function for destructor
-void Quadtree::clear(QuadtreeNode *&curNode){
-    if(curNode==NULL){
-        return;
-    }
+void Quadtree::clear(QuadtreeNode *&subRoot){
     
-    clear(curNode->nwChild);
-    clear(curNode->neChild);
-    clear(curNode->swChild);
-    clear(curNode->seChild);
+    if(subRoot == NULL) return;
+    clear(subRoot->nwChild);
+    clear(subRoot->neChild);
+    clear(subRoot->swChild);
+    clear(subRoot->seChild);
     
-    delete curNode;
-    curNode = NULL;
-    
-    return;
+    delete subRoot;
+    subRoot = NULL;
 }
 
 Quadtree const & Quadtree::operator=(Quadtree const &other){
@@ -104,123 +108,112 @@ Quadtree const & Quadtree::operator=(Quadtree const &other){
 }
 
 RGBAPixel Quadtree::getPixel(int x, int y) const{
-    if(root==NULL || x > resolution || y > resolution || x < 0 || y < 0){
+    if(x > resolution || x < 0 || y > resolution || y < 0 || root==NULL)
         return RGBAPixel();
-    }
-    else{
-        return getPixel(root, x, y, resolution);
-    }
+    
+    else return getPixel(root, x, y, resolution);
 }
 
 //helper function for getpixel
-RGBAPixel Quadtree::getPixel(QuadtreeNode const *curNode, int x, int y, int resolution)const{
-    if(curNode->nwChild == NULL || resolution == 1)
-        return curNode->element;
+RGBAPixel Quadtree::getPixel(QuadtreeNode const *subRoot, int x, int y, int resolution)const{
+    if(subRoot->nwChild == NULL || resolution == 1)
+        return subRoot->element;
     
     if(x < resolution/2 && y < resolution/2)
-        return getPixel(curNode->nwChild, x, y, resolution/2);
+        return getPixel(subRoot->nwChild, x, y, resolution/2);
     
     else if(x < resolution/2 && y >= resolution/2)
-        return getPixel(curNode->swChild, x, y-resolution/2, resolution/2);
+        return getPixel(subRoot->swChild, x, y-resolution/2, resolution/2);
     
     else if(x >= resolution/2 && y < resolution/2)
-        return getPixel(curNode->neChild, x-resolution/2, y, resolution/2);
+        return getPixel(subRoot->neChild, x-resolution/2, y, resolution/2);
     
-    else return getPixel(curNode->seChild, x-resolution/2, y-resolution/2, resolution/2);
+    else return getPixel(subRoot->seChild, x-resolution/2, y-resolution/2, resolution/2);
 }
 
 PNG Quadtree::decompress() const{
-    if(root==NULL) return PNG();
+    if(root == NULL) return PNG();
     
-    PNG result = PNG(resolution, resolution);
+    PNG ret = PNG(resolution, resolution);
     for (int i = 0; i < resolution; i++){
         for (int j = 0; j < resolution; j++){
-            *result(i, j) = getPixel(i, j);
+            *ret(i, j) = getPixel(i, j);
         }
     }
-    return result;
+    return ret;
 }
 
 //mp5.2
 void Quadtree::clockwiseRotate(){
-    if(root==NULL){
-        return;
-    }
+    // Rotates the Quadtree object's underlying image clockwise by 90 degrees.
+    if(root==NULL) return;
     clockwiseRotate(root);
 }
 
 //helper function for clockwiseRotate
-void Quadtree::clockwiseRotate(QuadtreeNode *curNode){
-    if(curNode->nwChild == NULL){
-        return;
-    }
+void Quadtree::clockwiseRotate(QuadtreeNode *subRoot){
     
-    QuadtreeNode *temp1 = curNode->nwChild;
-    QuadtreeNode *temp2 = curNode->neChild;
-    QuadtreeNode *temp3 = curNode->swChild;
-    QuadtreeNode *temp4 = curNode->seChild;
+    if(subRoot->nwChild == NULL) return;
     
-    curNode->nwChild = temp3;
-    curNode->neChild = temp1;
-    curNode->swChild = temp4;
-    curNode->seChild = temp2;
+    QuadtreeNode *temp1 = subRoot->nwChild;
+    QuadtreeNode *temp2 = subRoot->neChild;
+    QuadtreeNode *temp3 = subRoot->swChild;
+    QuadtreeNode *temp4 = subRoot->seChild;
     
-    clockwiseRotate(curNode->nwChild);
-    clockwiseRotate(curNode->neChild);
-    clockwiseRotate(curNode->swChild);
-    clockwiseRotate(curNode->seChild);
+    subRoot->nwChild = temp3;
+    subRoot->neChild = temp1;
+    subRoot->swChild = temp4;
+    subRoot->seChild = temp2;
+    
+    clockwiseRotate(subRoot->nwChild);
+    clockwiseRotate(subRoot->neChild);
+    clockwiseRotate(subRoot->swChild);
+    clockwiseRotate(subRoot->seChild);
 }
 
 void Quadtree::prune(int tolerance){
+    // Compresses the image this Quadtree represents.
     prune(tolerance, root);
 }
 
 //helper function for prune
-void Quadtree::prune(int tolerance, QuadtreeNode *curNode){
-    if(curNode==NULL){
+void Quadtree::prune(int tolerance, QuadtreeNode *subRoot){
+    
+    if(subRoot==NULL) return;
+    
+    if(subRoot->nwChild==NULL) return;
+    
+    bool prunable = checkPrune(subRoot, subRoot, tolerance);
+    if(prunable){
+        clear(subRoot->nwChild);
+        clear(subRoot->neChild);
+        clear(subRoot->swChild);
+        clear(subRoot->seChild);
         return;
     }
-    
-    if(curNode->nwChild==NULL){
-        return;
-    }
-    
-    if(checkPrune(curNode, curNode, tolerance)==true){
-        clear(curNode->nwChild);
-        clear(curNode->neChild);
-        clear(curNode->swChild);
-        clear(curNode->seChild);
-        
-        return;
-    }
-    
-    prune(tolerance, curNode->nwChild);
-    prune(tolerance, curNode->neChild);
-    prune(tolerance, curNode->swChild);
-    prune(tolerance, curNode->seChild);
-    
-    
+    prune(tolerance, subRoot->nwChild);
+    prune(tolerance, subRoot->neChild);
+    prune(tolerance, subRoot->swChild);
+    prune(tolerance, subRoot->seChild);
 }
 
 //helper function for prune
 bool Quadtree::checkPrune(QuadtreeNode const *root, QuadtreeNode const *curNode, int tolerance) const{
-    if(curNode->nwChild == NULL){
+    if (curNode->nwChild == NULL){
         int diff = (((root->element).red)-((curNode->element).red)) * (((root->element).red)-((curNode->element).red))
         + (((root->element).green)-((curNode->element).green)) * (((root->element).green)-((curNode->element).green))
         + (((root->element).blue)-((curNode->element).blue)) * (((root->element).blue)-((curNode->element).blue));
-        if(diff<=tolerance){
-            return true;
-        }
-        else{
-            return false;
-        }
+        if (diff <= tolerance) return true;
+        else return false;
     }
-    bool flag = true;
-    flag = checkPrune(root, curNode->nwChild, tolerance) && checkPrune(root, curNode->neChild, tolerance) && checkPrune(root, curNode->swChild, tolerance) && checkPrune(root, curNode->seChild, tolerance);
-    return flag;
+    bool ret = true;
+    ret = checkPrune(root, curNode->nwChild, tolerance) && checkPrune(root, curNode->neChild, tolerance)
+        && checkPrune(root, curNode->swChild, tolerance) && checkPrune(root, curNode->seChild, tolerance);
+    return ret;
 }
 
 int Quadtree::pruneSize(int tolerance) const{
+    // Returns a count of the total number of leaves the Quadtree would have if it were pruned as in the prune function.
     return pruneSize(root, tolerance);
 }
 
