59953278cb1c0334cbe143512eb0811d84793975
zzhan145 <zzhan145@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-11-27 23:15:39 +0000

    mp7.1 submission

--- zzhan145/mp7/dsets.h
+++ zzhan145/mp7/dsets.h
@@ -1,1 +1,22 @@
 /* Your code here! */
+
+#ifndef DSETS_H
+#define DSETS_H
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class DisjointSets{
+public:
+    void addelements(int num);
+    int find(int elem);
+    void setunion(int a, int b);
+    
+private:
+    vector<int> elements;
+    
+};
+
+#endif
